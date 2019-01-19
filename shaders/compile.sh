68202d96c00ca27307482e36c44f745ee57b4723
Tyler Yath <tyleryath@berkeley.edu>
2019-01-19 11:33:40 -0800

    set up graphics pipeline

---
+++ shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/Tyylrrr/Developer/graphics/vulkansdk-macos-1.1.92.1/macOS/bin/glslangValidator -V shader.vert
+/Users/Tyylrrr/Developer/graphics/vulkansdk-macos-1.1.92.1/macOS/bin/glslangValidator -V shader.frag
